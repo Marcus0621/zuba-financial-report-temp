9db4ff14c6b247bc0ed40d8850711c59d5d62ccd
Marcus0621 <141002604+Marcus0621@users.noreply.github.com>
2024-12-28 02:29:01 +0000

    remove empty row

--- data_processing.py
+++ data_processing.py
@@ -39,16 +39,8 @@ def process_data(df_zuba):
             }
         )
 
-        # only to create empty row below
-        empty_row = pd.DataFrame(
-            {
-                'Owner Email': ['']
-            }
-        )
-
         # Add rows 
         output.append(total_row)  
-        output.append(empty_row)
 
     # Combine everything into a single DataFrame
     result_df = pd.concat(output, ignore_index=True)
