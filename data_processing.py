a2b373ed2ac79d4da351dd7e856f2378727fd940
Marcus0621 <141002604+Marcus0621@users.noreply.github.com>
2025-01-13 04:29:02 +0000

    Change title name

--- data_processing.py
+++ data_processing.py
@@ -35,23 +35,31 @@ def merge_dataset(zuba_result_df, ipay_result_df):
 
     # Calculate "Total Room Rate" and add it as a new column
     df_zuba["Total Room Rate"] = df_zuba["Room / Per Night / Price/每晚/價格"] * df_zuba["Day(s)"]
-    df_zuba['TA Commission - 10%'] = df_zuba["Total Room Rate"] * 10/100
-    df_zuba['Host Commission - 12%'] = df_zuba["Total Room Rate"] * 12/100
-    df_zuba['Total Amount After Commission'] = df_zuba["Total Room Rate"] - df_zuba['Host Commission - 12%']
-    df_zuba['Total Amount Pay to Host'] = df_zuba["Room / Cleaning Fee/清潔費"] + df_zuba["Room / Service Fee/服務費"] + df_zuba["Room / Tax/稅金"] + df_zuba['Total Amount After Commission']
+    df_zuba['TA Commission - 10% (RM)'] = df_zuba["Total Room Rate"] * 10/100
+    df_zuba['Host Commission - 12% (RM)'] = df_zuba["Total Room Rate"] * 12/100
+    df_zuba['Total Amount After Commission (RM)'] = df_zuba["Total Room Rate"] - df_zuba['Host Commission - 12% (RM)']
+    df_zuba['Total Amount Pay to Host (RM)'] = df_zuba["Room / Cleaning Fee/清潔費"] + df_zuba["Room / Service Fee/服務費"] + df_zuba["Room / Tax/稅金"] + df_zuba['Total Amount After Commission (RM)']
 
+    #Reorder Column 
     report_order = ['Booking No.', 'Confirmation Code', 'Booking Date', 'User', 'Booker Name', 'Booker Email', 'Check-in Date', 'Check-out Date', 'Property', 'Owner Email', 'Room Type',  
                     'Unit(s)', 'Total Guest', 'Room / Per Night / Price/每晚/價格', 'Day(s)', 'Total Room Rate', 'Room / Cleaning Fee/清潔費', 'Room / Service Fee/服務費', 'Total Amount',
-                    'ipay88 MDR', 'Payment Method_x', 'Status', 'TA Commission - 10%', 'Host Commission - 12%', 'Total Amount After Commission', 'Total Amount Pay to Host'] 
+                    'ipay88 MDR', 'Payment Method_x', 'Status', 'TA Commission - 10% (RM)', 'Host Commission - 12% (RM)', 'Total Amount After Commission (RM)', 'Total Amount Pay to Host (RM)'] 
 
     df_zuba = df_zuba[report_order]
+
+    #Change column name 
+    df_zuba.rename(columns={'Room / Per Night / Price/每晚/價格': 'Room / Per Night / Price/每晚/價格 (RM)'}, inplace=True)
+    df_zuba.rename(columns={'Room / Cleaning Fee/清潔費': 'Room / Cleaning Fee/清潔費 (RM)'}, inplace=True)
+    df_zuba.rename(columns={'Room / Service Fee/服務費': 'Room / Service Fee/服務費 (RM)'}, inplace=True)
+    df_zuba.rename(columns={'Room / Tax/稅金': 'Room / Tax/稅金 (RM)'}, inplace=True)
+    df_zuba.rename(columns={'Total Room Rate': 'Total Room Rate (RM)'}, inplace=True)
     
     # Sort the DataFrame by the 'Room Type' column
     df_zuba = df_zuba.sort_values(by='Room Type', ascending=True)
 
     # Group by "Owner Email" and calculate the sum 
-    total_host = df_zuba.groupby('Owner Email')['Total Amount Pay to Host'].sum().reset_index()
-    total_com = df_zuba.groupby('Owner Email')['Total Amount After Commission'].sum().reset_index()
+    total_host = df_zuba.groupby('Owner Email')['Total Amount Pay to Host (RM)'].sum().reset_index()
+    total_com = df_zuba.groupby('Owner Email')['Total Amount After Commission (RM)'].sum().reset_index()
 
     # Prepare a list to store the output DataFrame with totals rows
     output = []
@@ -63,8 +71,8 @@ def merge_dataset(zuba_result_df, ipay_result_df):
         total_row = pd.DataFrame(
             {
                 'Booking No.': ['Total'],
-                'Total Amount Pay to Host': [total_host.loc[total_host['Owner Email'] == owner_email, 'Total Amount Pay to Host'].values[0]],
-                'Total Amount After Commission': [total_com.loc[total_com['Owner Email'] == owner_email, 'Total Amount After Commission'].values[0]],
+                'Total Amount Pay to Host (RM)': [total_host.loc[total_host['Owner Email'] == owner_email, 'Total Amount Pay to Host (RM)'].values[0]],
+                'Total Amount After Commission (RM)': [total_com.loc[total_com['Owner Email'] == owner_email, 'Total Amount After Commission (RM)'].values[0]],
             }
         )
 
